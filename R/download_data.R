6b86d143ced8bb73239abfdc7cb2af670740ca5c
Rasmus Skytte Eriksen <RSKE@dksund.dk>
2021-01-12 16:29:10 +0100

    added the mode='wb' for use on windows machines

--- R/download_data.R
+++ R/download_data.R
@@ -13,8 +13,7 @@
 #' @importFrom readr read_delim locale
 #'
 #' @export
-download_data <- function(preprocess=TRUE,dir=NULL)
-{
+download_data <- function(preprocess=TRUE,dir=NULL) {
     url <- "https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata"
     webpage <- read_html(url)
     anchors <- html_nodes(webpage, "a")
@@ -24,8 +23,7 @@ download_data <- function(preprocess=TRUE,dir=NULL)
     MostRecentFile <- urls[[MostRecentFileNr]]
 
 
-    if(is.null(dir))
-    {
+    if(is.null(dir)) {
         td <- tempdir()
     } else {
         td <- dir
@@ -33,18 +31,18 @@ download_data <- function(preprocess=TRUE,dir=NULL)
     }
 
     ## TODO : On at least one windows machine, the flag mode="wb" must be added - this maybe sets write permissions?
-    download.file(MostRecentFile,file.path(td, "data.zip"))
-        
+    download.file(MostRecentFile, file.path(td, "data.zip"), mode="wb")
+
     unzip(file.path(td, "data.zip"), exdir=file.path(td,"data"))
     text <- gsub(" ", "", readLines(file.path(td, "data/Test_pos_over_time.csv")))
     dat <- read_delim(paste(text, collapse='\n'), delim=";", locale=locale(decimal_mark=",", grouping_mark='.'))
 
-    dat <- head(dat,-2)
+    dat <- head(dat, -2)
 
     ## Preprocess:
-    if(preprocess){
+    if (preprocess) {
         dat <- preprocess.data(dat)
     }
-    
+
     return(dat)
 }
